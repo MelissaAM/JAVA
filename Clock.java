c6f06a2720df775ffcd9e96d79607e47cc35939f
Стив <kenethra.74@gmail.com>
2023-09-02 19:47:32 -0600

    feat: number roman clock

--- Clock.java
+++ Clock.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
 import java.awt.Dimension;
 import java.awt.RenderingHints;
 import java.awt.geom.Path2D;
@@ -32,18 +32,37 @@ public class Clock extends JPanel {
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
     // Cuadrado
-    double squareSize = 300;
+    double squareSize = 200;
     double squareX = (PREF_W - squareSize) / 2.0;
-    double squareY = PREF_H * 3.0 / 4.0 + 20;
+    double squareY = PREF_H * 3.0 / 4.0 + 20; // Ajusta la posicion vertical
+                                              // aca
 
     g2.setPaint(Color.gray);
     g2.fillRect((int) squareX, (int) squareY, (int) squareSize, (int) squareSize);
+
+    // Circulo
     double circleSize = squareSize * 0.8; // Tamaño del círculo (80% del cuadrado)
     double circleX = squareX + (squareSize - circleSize) / 2.0;
     double circleY = squareY + (squareSize - circleSize) / 2.0;
 
-    g2.setPaint(Color.red); // Color del círculo
-    g2.fill(new Ellipse2D.Double(circleX, circleY, circleSize, circleSize));
+    g2.setPaint(Color.white); // Color del círculo
+    g2.fillOval((int) circleX, (int) circleY, (int) circleSize, (int) circleSize);
+
+    g2.setPaint(Color.black);
+    Font font = new Font("Times New Roman", Font.PLAIN, 24);
+    g2.setFont(font);
+
+    for (int i = 1; i <= 12; ++i) {
+      double angle = Math.toRadians(360 - (i * 30));
+      double x = circleX + circleSize / 2 * Math.cos(angle);
+      double y = circleY + circleSize / 2 * Math.sin(angle);
+
+      String numeral = convertToRoman(i);
+      int width = g2.getFontMetrics().stringWidth(numeral); 
+      int height = g2.getFontMetrics().getHeight(); 
+
+      g2.drawString(numeral, (int) x - width / 2, (int) y + height / 4);
+    }
 
     // Triangulo
     g2.setPaint(color);
@@ -59,17 +78,7 @@ public class Clock extends JPanel {
   }
 
   private String convertToRoman(int num) {
-    String[] romanSymbols = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C" };
-    int[] arabicValues = { 1, 4, 5, 9, 10, 40, 50, 90, 100 };
-    StringBuilder romanNumeral = new StringBuilder();
-
-    for (int i = arabicValues.length - 1; i >= 0; i--) {
-      while (num >= arabicValues[i]) {
-        romanNumeral.append(romanSymbols[i]);
-        num -= arabicValues[i];
-      }
-    }
-
-    return romanNumeral.toString();
+    String[] romanSymbols = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII" };
+    return romanSymbols[num - 1];
   }
 }
